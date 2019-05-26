b593d6da9d527a32cdffbf121f0bbb05cdab3148
Alex Timoshevskyi <alexander.timoshevsky@gmail.com>
2019-05-26 21:46:08 +0200

    markup

--- ch3demoproject/src/app/cicalert/cicalert.component.ts
+++ ch3demoproject/src/app/cicalert/cicalert.component.ts
@@ -1,26 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
-class CicAlert
-{
-    createdDate:string;
-    status:string;
-    fromValue:string;
-    toValue:string;
-    description:string;
-
-    public constructor(init?:Partial<CicAlert>) {
-      Object.assign(this, init);
-  }
-}
-
-class TaxStatus
-{
-  effactiveDate:string;
-  endDate:string;
-  docType:string;
+class ScFormData{
+  customerId: number;
   formType:string;
   ch4Status:string;
   ch3Status:string;
+
+  public constructor(init?: Partial<ScFormData>) {
+    Object.assign(this, init);
+  }
+}
+
+class ReferenceData {
+  customerID: number;
+  customerName: string;
+  address: string;
+
+
+  public constructor(init?: Partial<ReferenceData>) {
+    Object.assign(this, init);
+  }
+}
+
+
+class CicAlert {
+  
+  createdDate: string;
+  status: string;
+  fromValue: string;
+  toValue: string;
+  description: string;
+
+  public constructor(init?: Partial<CicAlert>) {
+    Object.assign(this, init);
+  }
+}
+
+class TaxStatus {
+  effactiveDate: string;
+  endDate: string;
+  docType: string;
+  formType: string;
+  ch4Status: string;
+  ch3Status: string;
 }
 
 @Component({
@@ -30,22 +52,25 @@ class TaxStatus
 })
 export class CicalertComponent implements OnInit {
 
-  cicAlertList:Array<CicAlert>;
+  referenceDataList: Array<ReferenceData>;
+  scFormData: ScFormData;
+  cicAlertList: Array<CicAlert>;
 
-  
-    
-  constructor() { 
-   
+
+
+  constructor() {
+
   }
 
   ngOnInit() {
 
     this.cicAlertList = new Array<CicAlert>();
-    this.cicAlertList.push(new CicAlert({createdDate:new Date().toISOString(), status :'Open',fromValue:'Szybowcowa 42',toValue:'Bulwar Dedala 10', description:'Mail address has been changed'}));
+    this.referenceDataList = new Array<ReferenceData>();
+    this.referenceDataList.push(new ReferenceData({ customerID:1, customerName:'Alex Timoshevskyi', address: 'Szybowcowa 42' }));
     //  cicAlertList = [
-  //    ,
-  //    new CicAlert({createdDate:Date.now.toString(), status :'Open',fromValue:'CA',toValue:'US', description:'US Country of citizenship'}),
-  //  ]
+    //    ,
+    //    new CicAlert({createdDate:Date.now.toString(), status :'Open',fromValue:'CA',toValue:'US', description:'US Country of citizenship'}),
+    //  ]
   }
 
 }
