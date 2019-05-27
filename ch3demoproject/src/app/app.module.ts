fe86008ae572785b9a61190630be2f662b349a41
Alex Timoshevskyi <alexander.timoshevsky@gmail.com>
2019-05-27 22:00:00 +0200

    implementation of CIC Alerts and reference data upload

--- ch3demoproject/src/app/app.module.ts
+++ ch3demoproject/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +9,13 @@ import { CicalertComponent } from './cicalert/cicalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent,  
     NavbarComponent,
     CicalertComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- ch3demoproject/src/app/cicalert/cicalert.component.ts
+++ ch3demoproject/src/app/cicalert/cicalert.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-class ScFormData{
-  customerId: number;
-  formType:string;
-  ch4Status:string;
-  ch3Status:string;
+
+class ScFormData {
+  loadedDate: string;
+  customerID: number;
+  formType: string;
+  ch4Status: string;
+  ch3Status: string;
+  eyResult: string;
+  validationIDNumber: string;
 
   public constructor(init?: Partial<ScFormData>) {
     Object.assign(this, init);
@@ -12,6 +16,7 @@ class ScFormData{
 }
 
 class ReferenceData {
+  loadedDate: string;
   customerID: number;
   customerName: string;
   address: string;
@@ -24,12 +29,13 @@ class ReferenceData {
 
 
 class CicAlert {
-  
+
+  customerID: number;
+  formType: string;
   createdDate: string;
   status: string;
   fromValue: string;
   toValue: string;
-  description: string;
 
   public constructor(init?: Partial<CicAlert>) {
     Object.assign(this, init);
@@ -52,25 +58,109 @@ class TaxStatus {
 })
 export class CicalertComponent implements OnInit {
 
-  referenceDataList: Array<ReferenceData>;
-  scFormData: ScFormData;
-  cicAlertList: Array<CicAlert>;
+  customerReferenceData: ReferenceData = new ReferenceData();
+  scFormData: ScFormData = new ScFormData();
 
+  referenceDataList: Array<ReferenceData>;
+  scFormDataList: Array<ScFormData>;
+
+  cicAlertList: Array<CicAlert>;
 
 
   constructor() {
 
   }
 
+  getScForm(customerID: number): ScFormData {
+    for (let i = 0; this.scFormDataList.length; i++) {
+      if (this.scFormDataList[i].customerID === customerID) {
+        return this.scFormDataList[i];
+      }
+    }
+    return null;
+  }
+
+  createCicAlert(from: string, to: string, scForm: ScFormData) {
+    let item = new CicAlert({
+      customerID: scForm.customerID,
+      formType: scForm.formType,
+      fromValue: from,
+      toValue: to,
+      createdDate: new Date().toLocaleTimeString(),
+      status: 'OPEN'
+    })
+
+
+    this.cicAlertList.push(
+      item
+    )
+    console.log(this.cicAlertList);
+  }
+
+  createTaxStatus() {
+
+  }
+
+  loadCustomer() {
+    let itemFound = false;
+    for (let i = 0; i < this.referenceDataList.length; i++) {
+      if (this.referenceDataList[i].customerID === this.customerReferenceData.customerID) {
+        itemFound = true;
+        let scForm = this.getScForm(this.referenceDataList[i].customerID);
+        if (scForm && scForm.eyResult.toLowerCase() === 'valid') {
+          this.createCicAlert(this.referenceDataList[i].address, this.customerReferenceData.address, scForm);
+        }
+
+        this.referenceDataList[i].address = this.customerReferenceData.address;
+        this.referenceDataList[i].customerName = this.customerReferenceData.customerName;
+        this.referenceDataList[i].loadedDate = new Date().toLocaleTimeString();
+      }
+    }
+    if (!itemFound) {
+      this.referenceDataList.push(new ReferenceData(
+        {
+          loadedDate: new Date().toLocaleTimeString(),
+          customerID: this.customerReferenceData.customerID,
+          customerName: this.customerReferenceData.customerName,
+          address: this.customerReferenceData.address
+        }));
+    }
+
+
+    this.customerReferenceData.customerID = 0;
+    this.customerReferenceData.customerName = '';
+    this.customerReferenceData.address = '';
+    this.customerReferenceData.loadedDate = '';
+  }
+
+  loadScForm() {
+
+    this.scFormDataList.push(
+      new ScFormData(
+        {
+          loadedDate: new Date().toLocaleTimeString(),
+          customerID: this.scFormData.customerID,
+          ch3Status: this.scFormData.ch3Status,
+          ch4Status: this.scFormData.ch4Status,
+          formType: this.scFormData.formType,
+          eyResult: this.scFormData.eyResult,
+          validationIDNumber: this.scFormData.validationIDNumber
+        }
+      ));
+
+    this.scFormData.customerID = 0;
+    this.scFormData.formType = '';
+    this.scFormData.ch3Status = '';
+    this.scFormData.ch4Status = '';
+    this.scFormData.eyResult = '';
+    this.scFormData.validationIDNumber = '';
+    this.scFormData.loadedDate = '';
+  }
   ngOnInit() {
 
     this.cicAlertList = new Array<CicAlert>();
     this.referenceDataList = new Array<ReferenceData>();
-    this.referenceDataList.push(new ReferenceData({ customerID:1, customerName:'Alex Timoshevskyi', address: 'Szybowcowa 42' }));
-    //  cicAlertList = [
-    //    ,
-    //    new CicAlert({createdDate:Date.now.toString(), status :'Open',fromValue:'CA',toValue:'US', description:'US Country of citizenship'}),
-    //  ]
+    this.scFormDataList = new Array<ScFormData>();
   }
 
 }
